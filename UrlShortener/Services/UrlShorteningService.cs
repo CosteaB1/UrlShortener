42e40ff8bdb1db8b9a90d9e65c482a3383d8b28e
Bivol Constantin <constantin.bivol@amdaris.com>
2024-08-29 17:34:35 +0300

    Add project files.

---
+++ UrlShortener/Services/UrlShorteningService.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Services
+{
+    public class UrlShorteningService
+    {
+        private const int NumberOfCharsInShortLink = 7;
+        private const string Alphabet=""
+    }
+}
